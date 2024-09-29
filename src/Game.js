3a714037d4ce150a8810ed3c526548c3b91ce190
eaddaa <eaddaa@gmail.com>
2024-09-29 16:40:12 +0000

    Resolve merge conflicts

--- src/Game.js
+++ src/Game.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import './Game.css';
 import rocketImage from './images/rocket.png'; // Rocket image
 import planetImage from './images/planet.png'; // Planet image
